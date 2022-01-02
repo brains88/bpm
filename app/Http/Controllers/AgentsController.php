67d583a9d183d02bbb0216ca360b0ab43ce1d152
geohomes <93206555+geohomes@users.noreply.github.com>
2022-01-02 17:57:41 +0100

    Updating agents Controller

--- app/Http/Controllers/AgentsController.php
+++ app/Http/Controllers/AgentsController.php
@@ -11,6 +11,6 @@ class AgentsController extends Controller
      */
     public function index()
     {
-        return view('agents.index');
+        return view('frontend.agents.index');
     }
 }
